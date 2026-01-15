779508ca1a9f5e69c6cc6847167bfcd807c9764a
ngodanghao2005 <hao.ngondhcrqh719@hcmut.edu.vn>
2026-01-16 00:29:50 +0700

    Refactor data loading and training functions

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from SCKD import *
 import argparse
 import os
+import random
 
 class SubsetDataset(Dataset):
     def __init__(self, base_dataset, indices):
@@ -11,9 +12,11 @@ class SubsetDataset(Dataset):
         return len(self.indices)
 
     def __getitem__(self, idx):
-        return self.base_dataset[self.indices[idx]]
+        # Trả về thêm idx để ánh xạ với mảng global_labels
+        img, target = self.base_dataset[self.indices[idx]]
+        return img, target, idx
 
-def get_dataloaders(batch_size=512, num_workers=4, name="cifar10"):
+def get_dataloaders_train(batch_size=512, num_workers=0, name="cifar10", shuffle=True, drop_last=True):
     # Augmentations (theo paper: crop, flip, jitter, grayscale)
     transform_train = transforms.Compose([
         transforms.RandomResizedCrop(32, scale=(0.2, 1.0)),
@@ -27,19 +30,75 @@ def get_dataloaders(batch_size=512, num_workers=4, name="cifar10"):
     
     if name == "cifar10":
         trainset = datasets.CIFAR10(root="./data", train=True, download=True, transform=transform_train)
-        # Chia lớp: 0–4 = known, 5–9 = novel
-        known_classes = list(range(0, 5))
         novel_classes = list(range(5, 10))
+        known_classes = list(range(0, 5))
     elif name == "cifar100_20":
         trainset = datasets.CIFAR100(root="./data", train=True, download=True, transform=transform_train)
-        # Chia lớp: 0–79 = known, 80–99 = novel
-        known_classes = list(range(0, 80))
         novel_classes = list(range(80, 100))
+        known_classes = list(range(0, 80))
     elif name == "cifar100_50":
         trainset = datasets.CIFAR100(root="./data", train=True, download=True, transform=transform_train)
-        # Chia lớp: 0–49 = known, 50–99 = novel
-        known_classes = list(range(0, 50))
         novel_classes = list(range(50, 100))
+        known_classes = list(range(0, 50))
+    elif name == "cifar100_80":
+        trainset = datasets.CIFAR100(root="./data", train=True, download=True, transform=transform_train)
+        novel_classes = list(range(20, 100))
+        known_classes = list(range(0, 20))
+    elif name == "imagenet_100":
+        trainset = datasets.CIFAR100(root="./data", train=True, download=True, transform=transform_train)
+        novel_classes = list(range(20, 100))
+        known_classes = list(range(0, 20))
+
+    indices_labeled = [i for i, (_, y) in enumerate(trainset) if y in known_classes]
+    indices_unlabeled = [i for i, (_, y) in enumerate(trainset) if y in novel_classes]
+
+    labeled_set = SubsetDataset(trainset, indices_labeled)
+    unlabeled_set = SubsetDataset(trainset, indices_unlabeled)
+    
+    print(f"Labeled set: {len(labeled_set)} samples, classes: {known_classes}")
+    print(f"Unlabeled set: {len(unlabeled_set)} samples, classes: {novel_classes}")
+
+    # Kiểm tra vài samples đầu tiên
+    for i in range(3):
+        _, label, _ = labeled_set[i]
+        print(f"Labeled sample {i}: class {label}")
+    for i in range(3):
+        _, label, _ = unlabeled_set[i]
+        print(f"Unlabeled sample {i}: class {label}")
+
+    # DataLoaders
+    loader_labeled = DataLoader(labeled_set, batch_size=batch_size,
+                                shuffle=shuffle, num_workers=num_workers, drop_last=drop_last)
+    loader_unlabeled = DataLoader(unlabeled_set, batch_size=batch_size,
+                                  shuffle=shuffle, num_workers=num_workers, drop_last=drop_last)
+
+    return loader_labeled, loader_unlabeled, known_classes, novel_classes
+
+def get_dataloaders_test(batch_size=512, num_workers=0, name="cifar100_50", train = True, shuffle=False, drop_last=False):
+    # Augmentations (theo paper: crop, flip, jitter, grayscale)
+    transform_test = transforms.Compose([
+        transforms.ToTensor(), # Chuyển đổi thành tensor
+        # Giả định ảnh 32x32 như CIFAR, không cần Resize/CenterCrop
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                            (0.2023, 0.1994, 0.2010)),
+    ])
+    
+    if name == "cifar10":
+        trainset = datasets.CIFAR10(root="./data", train=train, download=True, transform=transform_test)
+        novel_classes = list(range(5, 10))
+        known_classes = list(range(0, 5))
+    elif name == "cifar100_20":
+        trainset = datasets.CIFAR100(root="./data", train=train, download=True, transform=transform_test)
+        novel_classes = list(range(80, 100))
+        known_classes = list(range(0, 80))
+    elif name == "cifar100_50":
+        trainset = datasets.CIFAR100(root="./data", train=train, download=True, transform=transform_test)
+        novel_classes = list(range(50, 100))
+        known_classes = list(range(0, 50))
+    elif name == "cifar100_80":
+        trainset = datasets.CIFAR100(root="./data", train=train, download=True, transform=transform_test)
+        novel_classes = list(range(20, 100))
+        known_classes = list(range(0, 20))
     
 
     indices_labeled = [i for i, (_, y) in enumerate(trainset) if y in known_classes]
@@ -53,19 +112,19 @@ def get_dataloaders(batch_size=512, num_workers=4, name="cifar10"):
 
     # Kiểm tra vài samples đầu tiên
     for i in range(3):
-        _, label = labeled_set[i]
+        _, label, _ = labeled_set[i]
         print(f"Labeled sample {i}: class {label}")
     for i in range(3):
-        _, label = unlabeled_set[i]
+        _, label, _ = unlabeled_set[i]
         print(f"Unlabeled sample {i}: class {label}")
 
     # DataLoaders
     loader_labeled = DataLoader(labeled_set, batch_size=batch_size,
-                                shuffle=True, num_workers=num_workers, drop_last=True)
+                                shuffle=shuffle, num_workers=num_workers, drop_last=drop_last)
     loader_unlabeled = DataLoader(unlabeled_set, batch_size=batch_size,
-                                  shuffle=True, num_workers=num_workers, drop_last=True)
+                                  shuffle=shuffle, num_workers=num_workers, drop_last=drop_last)
 
-    return loader_labeled, loader_unlabeled
+    return loader_labeled, loader_unlabeled, known_classes, novel_classes
 
 def main():
     parser = argparse.ArgumentParser()
@@ -75,22 +134,24 @@ def main():
                         help="Path to Stage 2 checkpoint (skip Stage 2 if provided)")
     parser.add_argument("--checkpoint_dir", type=str, default="./checkpoints",
                         help="Directory to save checkpoints")
+    parser.add_argument("--dataset_name", type=str, default="cifar10")
     args = parser.parse_args()
-    
+    args.checkpoint_dir = os.path.join(args.checkpoint_dir, args.dataset_name)
     os.makedirs(args.checkpoint_dir, exist_ok=True)
     
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Using device: {device}")
     
+    loader_labeled, loader_unlabeled, known_classes, novel_classes = get_dataloaders_train(name=args.dataset_name)
+    loader_eval_labeled, loader_eval_unlabeled, _, _ = get_dataloaders_test(name=args.dataset_name, train=True, shuffle=False, drop_last=False)
+    newloader_known, newloader_novel, _, _ = get_dataloaders_test(name=args.dataset_name, train=False, shuffle=False, drop_last=False)
     # 1. Config và DataLoader
     mode = "cifar"
-    cl, cu = 5, 5
+    cl, cu = len(known_classes), len(novel_classes)
     E1, E2 = 100, 500
     
-    cfg_stage1 = dict(lr=0.4, min_lr=1e-3, warmup_epochs=10)
-    cfg_stage2 = dict(lr=0.4, min_lr=1e-3, warmup_epochs=10)
-
-    loader_labeled, loader_unlabeled = get_dataloaders(name="cifar10")
+    cfg_stage1 = dict(lr=0.05, min_lr=0.001, warmup_epochs=10)
+    cfg_stage2 = dict(lr=0.1, min_lr=0.005, warmup_epochs=10)
 
     # 2. Build model
     model = build_model(mode, cl, cu, device)
@@ -118,15 +179,13 @@ def main():
                 }
                 checkpoint_path = os.path.join(args.checkpoint_dir, f"stage1_epoch_{e+1}.pth")
                 torch.save(checkpoint, checkpoint_path)
-            
-        torch.save(model.state_dict(), os.path.join(args.checkpoint_dir, "stage1_final.pth"))
     else:
         print(f"\n=== Loading Stage 1 checkpoint from {args.resume_stage1} ===")
-        
+
         opt1, sch1 = get_optimizer_scheduler(model, mode, cfg_stage1, total_epochs=E1)
-        
+
         model = model.to(device)
-        checkpoint = torch.load(args.resume_stage1, map_location=device)
+        checkpoint = torch.load(args.resume_stage1, map_location=device, weights_only=False)
         model.load_state_dict(checkpoint['model_state_dict'])
         opt1.load_state_dict(checkpoint['optimizer_state_dict'])
         sch1.load_state_dict(checkpoint['scheduler_state_dict'])
@@ -148,93 +207,147 @@ def main():
                     'stats': stats
                 }
                 checkpoint_path = os.path.join(args.checkpoint_dir, f"stage1_epoch_{e+1}.pth")
-                torch.save(checkpoint, checkpoint_path)
-            
-        torch.save(model.state_dict(), os.path.join(args.checkpoint_dir, "stage1_final.pth"))
-
+                torch.save(checkpoint, checkpoint_path)        
+    
+    # Tạo replica encoder đóng băng
+    Er = clone_frozen_replica(model.encoder)
     # 5. Stage-2: discovery
     if args.resume_stage2 is None:
         print("\n=== Stage 2: Discovery Training ===")
-        criterion = SCKDLoss(alpha=0.1, beta=0.5, distill_temp=1.0, sinkhorn_iter=5, sinkhorn_eps=0.35)
+        criterion = SCKDLoss(alpha=0.1, beta=0.1, gamma=8.0, distill_temp=1.0)
         opt2, sch2 = get_optimizer_scheduler(model, mode, cfg_stage2, total_epochs=E2)
-        
+        print("\nReplica encoder cloned and frozen")
+        checkpoint = {
+            'Er_state_dict': Er.state_dict()
+        }
+        global_pseudo_labels = None
+
         for e in range(E2):
-            Er = clone_frozen_replica(model.encoder)
-            print("\nReplica encoder cloned and frozen")
-            stats = stage2_discovery_epoch(model, Er, loader_labeled, loader_unlabeled, opt2, criterion, device)
+            if (e + 1) > 250:
+                criterion.beta = 0.005
+            elif (e + 1) > 100:
+                criterion.beta = 0.01  # Giảm trọng số Loss N2K sau 100 epoch
+            if e == 0 or (e + 1) % 5 == 0:
+                print(f"--- Updating Pseudo-labels at Epoch {e+1} ---")
+                global_pseudo_labels = run_global_estep(model, loader_eval_unlabeled, cu, device)
+            stats = stage2_discovery_epoch(model, Er, loader_labeled, loader_unlabeled, opt2, criterion, global_pseudo_labels, device)
             sch2.step()
             
-            print(f"Stage-2 [Epoch {e+1:3d}/{E2}] "
-                f"Total={stats['loss_total']:.4f} "
-                f"CE={stats['loss_ce_total']:.4f} "
-                f"K2N={stats['loss_k2n']:.4f} "
-                f"N2K={stats['loss_n2k']:.4f}")
+            model.eval()
+            total_known_acc = 0.0
+            total_n_known = 0
+            with torch.no_grad():
+                for x, y, _ in loader_eval_labeled:
+                    x, y = x.to(device, non_blocking=True), y.to(device, non_blocking=True)
+                    _, lhl, _ = model(x)
+                    pred_known = lhl.argmax(1)
+                    total_known_acc += int((pred_known == y).sum().item())
+                    total_n_known += y.size(0)
+                stats["known_acc"] = total_known_acc / max(1, total_n_known)
+            eval_stats = evaluate_training_subset(model, loader_eval_unlabeled, num_novel_classes=cu, device=device)
+            stats["novel_acc"] = eval_stats["acc_unlabeled"]
+            model.train()
             
+            print(f"Stage-2 [Epoch {e+1:3d}/{E2}] Beta={criterion.beta} "
+                f"LossTotal={stats['loss_total']:.4f} "
+                f"LossCE={stats['loss_ce_total']:.4f} "
+                f"LossK2N={stats['loss_k2n']:.4f} "
+                f"LossN2K={stats['loss_n2k']:.4f} "
+                f"LossCDC={stats['loss_cdc']:.4f} "
+                f"KnownAcc={stats['known_acc']:.4f} "
+                f"NovelAcc={stats['novel_acc']:.4f} ")
+
             if (e + 1) % 50 == 0:  # Lưu mỗi 50 epoch
-                checkpoint = {
-                    'epoch': e + 1,
-                    'model_state_dict': model.state_dict(),
-                    'optimizer_state_dict': opt2.state_dict(),
-                    'scheduler_state_dict': sch2.state_dict(),
-                    'stats': stats,
-                    'Er_state_dict': Er.state_dict()  # Lưu cả replica encoder
-                }
+                checkpoint["epoch"] = e + 1
+                checkpoint["model_state_dict"] = model.state_dict()
+                checkpoint["optimizer_state_dict"] = opt2.state_dict()
+                checkpoint["scheduler_state_dict"] = sch2.state_dict()
+                checkpoint["stats"] = stats
                 checkpoint_path = os.path.join(args.checkpoint_dir, f"stage2_epoch_{e+1}.pth")
                 torch.save(checkpoint, checkpoint_path)
-            
-        torch.save(model.state_dict(), os.path.join(args.checkpoint_dir, "stage2_final.pth"))
+                eval_stats2 = evaluate_testing_subset(model, newloader_known, newloader_novel, cl, cu, device=device)
+                print("Known:", eval_stats2["Acc_Known_Classes"])
+                print("Novel:", eval_stats2["Clustering_Acc_Novel_Classes"])
+                print("All:", eval_stats2["Acc_Overall"])
     else:
         print(f"\n=== Loading Stage 2 checkpoint from {args.resume_stage2} ===")
 
-        criterion = SCKDLoss(alpha=0.1, beta=0.5, distill_temp=1.0, sinkhorn_iter=5, sinkhorn_eps=0.35)
+        criterion = SCKDLoss(alpha=0.1, beta=0.1, gamma=8.0, distill_temp=1.0)
         opt2, sch2 = get_optimizer_scheduler(model, mode, cfg_stage2, total_epochs=E2)
         
         model = model.to(device)
-        checkpoint = torch.load(args.resume_stage2, map_location=device)
+        checkpoint = torch.load(args.resume_stage2, map_location=device, weights_only=False)
         model.load_state_dict(checkpoint['model_state_dict'])
         opt2.load_state_dict(checkpoint['optimizer_state_dict'])
         sch2.load_state_dict(checkpoint['scheduler_state_dict'])
-        Er = clone_frozen_replica(model.encoder)
-        
-        # for p in Er.parameters():
-        #     p.requires_grad = False
+        Er.load_state_dict(checkpoint['Er_state_dict'])
         
         start_epoch = checkpoint['epoch']
+        print("\nReplica encoder cloned and frozen")
+        global_pseudo_labels = run_global_estep(model, loader_eval_unlabeled, cu, device)
         
         for e in range(start_epoch, E2):
-            if e != start_epoch:
-                Er = clone_frozen_replica(model.encoder)
-            print("\nReplica encoder cloned and frozen")
-            stats = stage2_discovery_epoch(model, Er, loader_labeled, loader_unlabeled, opt2, criterion, device)
+            if (e + 1) > 250:
+                criterion.beta = 0.005
+            elif (e + 1) > 100:
+                criterion.beta = 0.01  # Giảm trọng số Loss N2K sau 100 epoch
+            if e == 0 or (e + 1) % 5 == 0:
+                print(f"--- Updating Pseudo-labels at Epoch {e+1} ---")
+                global_pseudo_labels = run_global_estep(model, loader_eval_unlabeled, cu, device)            
+            stats = stage2_discovery_epoch(model, Er, loader_labeled, loader_unlabeled, opt2, criterion, global_pseudo_labels, device)
             sch2.step()
             
-            print(f"Stage-2 [Epoch {e+1:3d}/{E2}] "
-                f"Total={stats['loss_total']:.4f} "
-                f"CE={stats['loss_ce_total']:.4f} "
-                f"K2N={stats['loss_k2n']:.4f} "
-                f"N2K={stats['loss_n2k']:.4f}")
+            model.eval()
+            total_known_acc = 0.0
+            total_n_known = 0
+            with torch.no_grad():
+                for x, y, _ in loader_eval_labeled:
+                    x, y = x.to(device, non_blocking=True), y.to(device, non_blocking=True)
+                    _, lhl, _ = model(x)
+                    pred_known = lhl.argmax(1)
+                    total_known_acc += int((pred_known == y).sum().item())
+                    total_n_known += y.size(0)
+                stats["known_acc"] = total_known_acc / max(1, total_n_known)
+            eval_stats = evaluate_training_subset(model, loader_eval_unlabeled, num_novel_classes=cu, device=device)
+            stats["novel_acc"] = eval_stats["acc_unlabeled"]
+            model.train()
             
+            print(f"Stage-2 [Epoch {e+1:3d}/{E2}] Beta={criterion.beta} "
+                f"LossTotal={stats['loss_total']:.4f} "
+                f"LossCE={stats['loss_ce_total']:.4f} "
+                f"LossK2N={stats['loss_k2n']:.4f} "
+                f"LossN2K={stats['loss_n2k']:.4f} "
+                f"LossCDC={stats['loss_cdc']:.4f} "
+                f"KnownAcc={stats['known_acc']:.4f} "
+                f"NovelAcc={stats['novel_acc']:.4f} ")
+
             if (e + 1) % 50 == 0:  # Lưu mỗi 50 epoch
-                checkpoint = {
-                    'epoch': e + 1,
-                    'model_state_dict': model.state_dict(),
-                    'optimizer_state_dict': opt2.state_dict(),
-                    'scheduler_state_dict': sch2.state_dict(),
-                    'stats': stats,
-                    'Er_state_dict': Er.state_dict()  # Lưu cả replica encoder
-                }
+                checkpoint["epoch"] = e + 1
+                checkpoint["model_state_dict"] = model.state_dict()
+                checkpoint["optimizer_state_dict"] = opt2.state_dict()
+                checkpoint["scheduler_state_dict"] = sch2.state_dict()
+                checkpoint["stats"] = stats
                 checkpoint_path = os.path.join(args.checkpoint_dir, f"stage2_epoch_{e+1}.pth")
                 torch.save(checkpoint, checkpoint_path)
-            
-        torch.save(model.state_dict(), os.path.join(args.checkpoint_dir, "stage2_final.pth"))
+                eval_stats2 = evaluate_testing_subset(model, newloader_known, newloader_novel, cl, cu, device=device)
+                print("Known:", eval_stats2["Acc_Known_Classes"])
+                print("Novel:", eval_stats2["Clustering_Acc_Novel_Classes"])
+                print("All:", eval_stats2["Acc_Overall"])
         
     print("\n=== Training Completed ===")
     print("Model is ready for evaluation on novel classes")
-    # -------- Evaluation --------
-    print("\n=== Evaluation on Unlabeled Subset ===")
-    _, newloader_unlabeled = get_dataloaders()
-    eval_stats = evaluate_task_aware(model, newloader_unlabeled, num_novel_classes=cu, device=device)
+
+    print("\n=== Evaluation on Unlabeled Training Subset ===")
+    _, newloader_unlabeled, _, _ = get_dataloaders_test(name=args.dataset_name, train=True, shuffle=False, drop_last=False)
+    eval_stats = evaluate_training_subset(model, newloader_unlabeled, num_novel_classes=cu, device=device)
     print("Unlabeled Clustering Acc:", eval_stats["acc_unlabeled"])
-        
+    
+    print("\n=== Evaluation on Testing Subset ===")
+    
+    eval_stats2 = evaluate_testing_subset(model, newloader_known, newloader_novel, cl, cu, device=device)
+    print("Known:", eval_stats2["Acc_Known_Classes"])
+    print("Novel:", eval_stats2["Clustering_Acc_Novel_Classes"])
+    print("All:", eval_stats2["Acc_Overall"])
+
 if __name__ == "__main__":
     main()
